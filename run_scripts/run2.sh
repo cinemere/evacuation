2391765dd3b80e78c9f004f9d89f4a5b942f0a0f
cinemere <klepach.aa@phystech.edu>
2023-11-12 18:28:56 +0300

    fix run script (negative init rew)

--- run_scripts/run2.sh
+++ run_scripts/run2.sh
@@ -34,7 +34,8 @@ reward_states_isfollowreward=(True False)
 reward_names_isfollowreward=("followrew" "nofollowrew")
 
 reward_states_intrinsicrewardcoef=("0." "1.")
-reward_initrew=("0." "-1.")
+# reward_initrew=("0." "-1.")
+reward_initrew=("-1.")
 alpha_states=("2" "3" "4" "5")
 noisecoef_states=(".2" ".5" ".8")
 ensldegree_states=("1." ".5" ".1")
@@ -60,11 +61,25 @@ for i_emb in ${!emb_states[@]}; do
                         
                             for ensldegree in ${ensldegree_states[@]}; do
 
+                                echo ""; echo "----------------------------------------------------------------"; echo "";
 
                                 exp_name="n${n_ped}_${emb_name}_${exitrew_name}_${follrew_name}_intrrew-${intrrew}_initrew-${initrew}_alpha-${alpha}_noise-${noisecoef}_ensldegree-${ensldegree}"
                                 echo "Starting: $exp_name"
-                                echo "python main.py --exp-name $exp_name -n $n_ped --learn-timesteps $learn_timesteps -e $emb_state --is-new-exiting-reward $exitrew_state --is-new-followers-reward $follrew_state --intrinsic-reward-coef $intrrew --init-reward-each-step $initrew--alpha $alpha --noise-coef $noisecoef --enslaving-degree $ensldegree"
-                                python main.py --exp-name $exp_name -n $n_ped --learn-timesteps $learn_timesteps -e $emb_state --is-new-exiting-reward $exitrew_state --is-new-followers-reward $follrew_state --intrinsic-reward-coef $intrrew --init-reward-each-step $initrew --alpha $alpha --noise-coef $noisecoef --enslaving-degree $ensldegree &
+
+                                env_setup="-n ${n_ped} --noise-coef ${noisecoef} --enslaving-degree ${ensldegree}"
+                                echo "Env params: ${env_setup}"
+
+                                reward_setup="--is-new-exiting-reward ${exitrew_state} --is-new-followers-reward ${follrew_state} --intrinsic-reward-coef ${intrrew} --init-reward-each-step=${initrew}"
+                                echo "Reward params: ${reward_setup}"
+                                
+                                learn_setup="--learn-timesteps ${learn_timesteps} -e ${emb_state} --alpha ${alpha}"
+                                echo "Learning experiment params: ${learn_setup}"
+
+                                query="--exp-name ${exp_name} ${reward_setup} ${env_setup} ${learn_setup}"
+                                echo ""; echo "All params:"; echo ""; echo "---->    ${query}"; echo ""
+                            
+                                python3 main.py $query
+
                             done
                             
                             wait
