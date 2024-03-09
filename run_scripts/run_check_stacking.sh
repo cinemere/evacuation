3480ea54fe4f4c4eea079dd8dc90a83906f9cc60
cinemere <klepach.aa@phystech.edu>
2024-03-09 16:50:34 +0300

    run script for sac and stacking (grav)

---
+++ run_scripts/run_check_stacking.sh
@@ -0,0 +1,17 @@
+common_args="-e true --init-reward-each-step=-1. --intrinsic-reward-coef 0"
+
+query1="--exp-name test-stacking-2-grav-ppo --num-obs-stacks 2 --origin ppo ${common_args}"
+query2="--exp-name test-stacking-1-grav-ppo --num-obs-stacks 1 --origin ppo ${common_args}"
+query3="--exp-name test-stacking-2-grav-sac --num-obs-stacks 2 --origin sac ${common_args}"
+query4="--exp-name test-stacking-1-grav-sac --num-obs-stacks 1 --origin sac ${common_args}"
+
+echo python3 main.py $query1
+echo python3 main.py $query2
+echo python3 main.py $query3
+echo python3 main.py $query4
+
+python3 main.py $query1 &
+python3 main.py $query2 &
+python3 main.py $query3 &
+python3 main.py $query4 &
+wait
