bc7c3fee3a1676b044f233d6f82aeacfd9bdd6eb
Albina Klepach <klepach.aa@phystech.edu>
2024-07-29 22:26:25 +0300

    upd run scripts

--- run_scripts/learn_transformer_emb_2.sh
+++ run_scripts/learn_transformer_emb_2.sh
@@ -1,9 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=evac_transformer
+#SBATCH --output=evac_transformer_log.txt
+#SBATCH --ntasks=1
+#SBATCH --time=600:00
+#SBATCH --mem-per-cpu=100
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=htc
+
 export DEVICE="cpu"
 export WANDB_PROJECT="evacuation_june"
 export WANDB_ENTITY="albinakl"
+export WANDB_API_KEY="5e7ca36a9a2b12b4cda477f8047ff713694b350e"
 
 EXPERIMENT_NAME="transformer-emb"
 
+#nodes to choose from: gpu, gpu_devel, htc, mem
+# srun -p htc -n 4 pwd
 
 python3 src/main.py \
 --env.experiment-name $EXPERIMENT_NAME \
@@ -11,7 +24,7 @@ python3 src/main.py \
 --env.noise-coef 0.2 \
 --env.enslaving-degree 1. \
 --env.is-new-exiting-reward \
---env.is-new-followers-reward \
+--env.no-is-new-followers-reward \
 --env.intrinsic-reward-coef 0. \
 --env.giff-freq 500 \
 --env.wandb-enabled \
